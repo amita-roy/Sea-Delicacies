444aa939ff167e75f7fa5a99cd8ddd756d02eb42
Amita-Roy <sardana.amita2@gmail.com>
2021-01-19 17:42:31 +0100

    Add style and css loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  devServer: {
+    contentBase: './dist',
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
