3439361b99fa1516c3e89ca08fecc28ac9b2c63c
Amita-Roy <sardana.amita2@gmail.com>
2021-01-20 14:50:05 +0100

    Add about component

---
+++ src/about.js
@@ -0,0 +1,7 @@
+export default function about() {
+  const aboutUs = document.createElement('div');
+
+  aboutUs.classList.add('about');
+
+  return aboutUs;
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import navbar from './nav.js';
 import header from './header.js';
+import about from './about.js';
 import './style.css';
 
 const mainContainer = document.querySelector('#content');
 mainContainer.appendChild(navbar());
 mainContainer.appendChild(header());
+mainContainer.appendChild(about());
