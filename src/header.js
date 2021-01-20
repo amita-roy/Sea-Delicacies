f1c3a88dd43d104b4155fe79a6e9c130796e9bed
Amita-Roy <sardana.amita2@gmail.com>
2021-01-20 10:40:52 +0100

    Add left side content for header

--- src/header.js
+++ src/header.js
@@ -1,7 +1,31 @@
+const leftContainer = () => {
+  const lc = document.createElement('div');
+  const heading = document.createElement('h1');
+  const description = document.createElement('p');
+  const cta = document.createElement('button');
+
+  lc.classList.add('leftHead');
+
+  heading.innerHTML = 'We Serve Love from the Ocean';
+  description.innerHTML =
+    'There is no better dish than the one prepared instantly especially for you. That is the specialty of SEA DELICACIES';
+
+  cta.innerHTML = 'Menu';
+
+  lc.appendChild(heading);
+  lc.appendChild(description);
+  lc.appendChild(cta);
+  return lc;
+};
+
 module.exports = function header() {
   const head = document.createElement('div');
 
+  const rightContainer = document.createElement('div');
+
   head.classList.add('header');
+
+  head.appendChild(leftContainer());
 
   return head;
 };
