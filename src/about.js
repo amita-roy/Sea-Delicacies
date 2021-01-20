1e2d4ec51776508ea8106167585230261eb30780
Amita-Roy <sardana.amita2@gmail.com>
2021-01-20 15:28:06 +0100

    Add about section with all the content

--- src/about.js
+++ src/about.js
@@ -1,7 +1,42 @@
+import aboutDesc from './aboutDesc.js';
+import aboutImage from './about1.png';
+
+const content = () => {
+  const rc = document.createElement('div');
+
+  const heading = document.createElement('h3');
+  const description = document.createElement('p');
+  const cta = document.createElement('button');
+
+  rc.classList.add('content');
+
+  heading.innerHTML = 'About Us';
+  description.innerHTML = aboutDesc();
+
+  cta.innerHTML = 'View More';
+
+  rc.appendChild(heading);
+  rc.appendChild(description);
+  rc.appendChild(cta);
+  return rc;
+};
+const imageContainer = () => {
+  const lc = document.createElement('div');
+
+  const image = new Image();
+  image.src = aboutImage;
+
+  lc.appendChild(image);
+  return lc;
+};
+
 export default function about() {
   const aboutUs = document.createElement('div');
 
   aboutUs.classList.add('about');
+
+  aboutUs.appendChild(imageContainer());
+  aboutUs.appendChild(content());
 
   return aboutUs;
 }
---
+++ src/aboutDesc.js
@@ -0,0 +1,2 @@
+module.exports = aboutDesc = () =>
+  'Welcome to Sea Delicacies. We have been providing fresh, fast, and friendly sea food. Sea Delicacies isn’t just about authentic sea food, it specializes in high standard, fresh and healthy food with affordable prices. Our food is cooked fresh to the customer’s order and taste. You are guaranteed the freshness of your order.';
