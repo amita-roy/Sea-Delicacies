3e16ce677b27cab9337ecd406727ffbaec74df52
Amita-Roy <sardana.amita2@gmail.com>
2021-01-21 14:37:28 +0100

    Comlete menu page with style and content

--- src/itemCard.js
+++ src/itemCard.js
@@ -1,22 +1,35 @@
 export default (imageUrl, name, price, description) => {
   const card = document.createElement('div');
+  const cardImage = document.createElement('div');
+  const cardDetails = document.createElement('div');
+  const cardDetailsTop = document.createElement('div');
   const dishName = document.createElement('p');
   const dishPrice = document.createElement('p');
   const dishDescription = document.createElement('p');
 
   card.classList.add('card');
+  cardImage.classList.add('cardImage');
+  cardDetails.classList.add('cardDetails');
+  cardDetailsTop.classList.add('cardDetailsTop');
+  dishName.classList.add('dishName');
+  dishPrice.classList.add('dishPrice');
+  dishDescription.classList.add('dishDescription');
 
-  const dishImage = new Image();
-  dishImage.src = imageUrl;
+  cardImage.style.backgroundImage = 'url(' + imageUrl + ')';
 
   dishName.innerText = name;
   dishPrice.innerText = price;
   dishDescription.innerText = description;
 
-  card.appendChild(dishImage);
-  card.appendChild(dishName);
-  card.appendChild(dishPrice);
-  card.appendChild(dishDescription);
+  cardDetailsTop.appendChild(dishName);
+  cardDetailsTop.appendChild(dishPrice);
+
+  cardDetails.appendChild(cardDetailsTop);
+  cardDetails.appendChild(dishDescription);
+
+  card.appendChild(cardImage);
+
+  card.appendChild(cardDetails);
 
   return card;
 };
--- src/menu.js
+++ src/menu.js
@@ -4,16 +4,22 @@ import ItemCard from './itemCard';
 
 export default function menu() {
   const mainContainer = document.querySelector('#content');
+  const menuContent = document.createElement('div');
   const itemGrid = document.createElement('div');
 
+  menuContent.classList.add('menu');
+  itemGrid.classList.add('itemList');
+  menuContent.id = 'menu';
   menuItems.forEach((item) => {
     itemGrid.appendChild(
       ItemCard(item.url, item.name, item.price, item.description)
     );
   });
   mainContainer.innerHTML = '';
-  mainContainer.appendChild(navbar('opaque'));
-  mainContainer.appendChild(itemGrid);
 
+  menuContent.appendChild(navbar('opaque'));
+  menuContent.appendChild(itemGrid);
+
+  mainContainer.appendChild(menuContent);
   return mainContainer;
 }
