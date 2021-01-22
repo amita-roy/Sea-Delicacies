ae74a412ba9f474dc104ca07f438ca2a78ce4a92
Amita-Roy <sardana.amita2@gmail.com>
2021-01-22 09:26:45 +0100

    Add core, support and contact

--- src/contactDeatils.js
+++ src/contactDeatils.js
@@ -1,3 +1,52 @@
+const coreLinks = (paren) => {
+  const head = document.createElement('h4');
+  const home = document.createElement('p');
+  const about = document.createElement('p');
+  const menu = document.createElement('p');
+
+  head.innerText = 'Core Links';
+  home.innerText = 'Home';
+  about.innerText = 'About Us';
+  menu.innerText = 'Menu';
+
+  paren.appendChild(head);
+  paren.appendChild(home);
+  paren.appendChild(about);
+  paren.appendChild(menu);
+};
+const supportLinks = (paren) => {
+  const head = document.createElement('h4');
+  const faq = document.createElement('p');
+  const tc = document.createElement('p');
+  const cs = document.createElement('p');
+
+  head.innerText = 'Support';
+  faq.innerText = 'FAQ';
+  tc.innerText = 'Terms & Conditions';
+  cs.innerText = 'Customer Support';
+
+  paren.appendChild(head);
+  paren.appendChild(faq);
+  paren.appendChild(tc);
+  paren.appendChild(cs);
+};
+const conLinks = (paren) => {
+  const head = document.createElement('h4');
+  const phone = document.createElement('p');
+  const email = document.createElement('p');
+  const add = document.createElement('p');
+
+  head.innerText = 'Contact Us';
+  phone.innerText = '(+46) 781 1234 20';
+  email.innerText = 'amita.roy@workmail.com';
+  add.innerText = 'Online lane 24 Stockholm, Sweden';
+
+  paren.appendChild(head);
+  paren.appendChild(phone);
+  paren.appendChild(email);
+  paren.appendChild(add);
+};
+
 export default function details() {
   const contactDetails = document.createElement('div');
   const linksContainer = document.createElement('div');
@@ -7,9 +56,10 @@ export default function details() {
 
   linksContainer.classList.add('contactLinksContainer');
   contactDetails.classList.add('contactDetails');
-  core.innerText = 'All Core links';
-  support.innerText = 'All Support links';
-  contactUs.innerText = 'All contact links';
+
+  coreLinks(core);
+  supportLinks(support);
+  conLinks(contactUs);
 
   linksContainer.appendChild(core);
   linksContainer.appendChild(support);
