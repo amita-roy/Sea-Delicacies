ceaba8b31aa965138dd8aef2537b631317fbe6b8
Amita-Roy <sardana.amita2@gmail.com>
2021-01-22 07:53:26 +0100

    Update contact details section

--- src/contactDeatils.js
+++ src/contactDeatils.js
@@ -1,6 +1,21 @@
 export default function details() {
   const contactDetails = document.createElement('div');
+  const linksContainer = document.createElement('div');
+  const core = document.createElement('div');
+  const support = document.createElement('div');
+  const contactUs = document.createElement('div');
 
+  linksContainer.classList.add('contactLinksContainer');
   contactDetails.classList.add('contactDetails');
+  core.innerText = 'All Core links';
+  support.innerText = 'All Support links';
+  contactUs.innerText = 'All contact links';
+
+  linksContainer.appendChild(core);
+  linksContainer.appendChild(support);
+  linksContainer.appendChild(contactUs);
+
+  contactDetails.appendChild(linksContainer);
+
   return contactDetails;
 }
